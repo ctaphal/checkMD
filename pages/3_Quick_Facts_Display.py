15b35c90d23b22680abf9492d345e2ef8a720a8e
ctaphal <67525176+ctaphal@users.noreply.github.com>
2023-11-05 01:35:26 +0000

    Display page update

--- pages/3_Quick_Facts_Display.py
+++ pages/3_Quick_Facts_Display.py
@@ -13,55 +13,102 @@
 # limitations under the License.
 
 from typing import Any
-
 import numpy as np
-
-import streamlit as st
-from streamlit.hello.utils import show_code
-
 import streamlit as st
 from datetime import datetime as dt
-from storage import or_items
+import altair as alt
+import pandas as pd
+import io
+import xlsxwriter
 
-if "mytsks" not in st.session_state:
-    st.session_state.mytsks = []
+from storage import allergies_list
+from storage import medical_hist
+from storage import meds_list
+from storage import surgical_site
+from storage import blood_type
 
-if "tskclk" not in st.session_state:
-    st.session_state.tskclk = []
+def data_frame_demo():
+        st.subheader("Allergies")
+        allg_df = pd.DataFrame(allergies_list, columns="Allergies")
+        st.dataframe(allg_df)
 
-if "chkarr" not in st.session_state:
-    st.session_state.chkarr = []
+        st.subheader("Medical History")
+        medHx_df = pd.DataFrame(medical_hist, columns="Medical History")
+        st.dataframe(medHx_df)
 
-if "rerun" not in st.session_state:
-    st.session_state.rerun = False
+        st.subheader("Medications")
+        meds_df = pd.DataFrame(meds_list, columns="Medications")
+        st.dataframe(meds_df)
 
+        st.subheader("Surgical Site(s)")
+        ssite_df = pd.DataFrame(surgical_site, columns="Surgical Site(s)")
+        st.dataframe(ssite_df)
+        
+        st.subheader("Blood Type")
+        bt_df = pd.DataFrame(blood_type, columns="Blood Type")
+        st.dataframe(bt_df)
 
-def cmpltTask(or_item):
-    idx = st.session_state.mytsks.index(or_item)
-    st.session_state.chkarr[idx] = not st.session_state.chkarr[idx]
-    st.session_state.rerun = True
+        save_to_Excel(allg_df, medHx_df, meds_df, ssite_df, bt_df)
 
-def listTasks():
-    st.session_state.tskclk = []
-    st.markdown("")
-    st.subheader("Post-Op: ")
-    st.caption("*Check off each item after it's been accounted for post-op*")
-    for or_item in or_items:
-        st.checkbox(f"- **{or_item}**")
+def save_to_Excel(allg_df, medHx_df, meds_df, ssite_df, bt_df):
+    # Write files to in-memory strings using BytesIO
+    # See: https://xlsxwriter.readthedocs.io/workbook.html?highlight=BytesIO#constructor
+    output = io.BytesIO()
+    workbook = xlsxwriter.Workbook(output, {'in_memory': True})
+    worksheet = workbook.add_worksheet("OR Surgery Record")
 
-st.title("OR Surgical Tool Tracker")
+    if pt_info!=[]:
+        worksheet.write('A1', "Pt Info")
+        for i in range(0, len(pt_cols)):
+            cell = 'A'+str(i+2)
+            worksheet.write(cell, str(pt_cols[i]))
+        for i in range(0, len(pt_info[0])):
+            cell = 'B'+str(i+2)
+            worksheet.write(cell, str(pt_info[0][i]))
 
-if st.session_state.rerun == True:
-    st.session_state.rerun = False
-    st.experimental_rerun()
+    if diagnoses_store!={}:
+        worksheet.write('D1', "Diagnoses")
+        worksheet.write('E1', "Notes")
+        count = 0
+        for diag in (diag_keys):
+            cell = 'D'+str(count+2)
+            worksheet.write(cell, str(diag))
+            count = count+1
+        count = 0
+        for val in (diag_vals):
+            cell = 'E'+str(count+2)
+            worksheet.write(cell, str(val))
+            count = count+1
 
-else:
-    st.header("Pre-Op: ")
-    item = st.text_input("Enter items brought into the OR (one at a time): ", value="")
-    if st.button('Add Item'):
-        if item != "":
-            or_items.append(item)
-            st.session_state.mytsks.append(item)
-            st.session_state.chkarr.append(False)
+    if meds_store!=[]:
+        for i in range(0, len(meds_store)):
+            worksheet.write('G1', "Meds")
+            cell = 'G'+str(i+2)
+            worksheet.write(cell, str(meds_store[i]))
 
-listTasks()
+    if vitals_store!=[]:
+        worksheet.write('I1', "Vitals")
+        for i in range(0, len(vitals_cols)):
+            cell = 'I'+str(i+2)
+            worksheet.write(cell, str(vitals_cols[i]))
+
+        asc = 74
+        for i in range(0, len(vitals_store)):
+            for j in range(0, len(vitals_store[0])):
+                cell = str(chr(asc)+str(j+2))
+                worksheet.write(cell, str(vitals_store[i][j]))
+            asc = asc+1
+
+    workbook.close()
+    st.download_button(
+        label="Download as Excel Sheet",
+        data=output.getvalue(),
+        file_name="DowntimeProRecord.xlsx",
+        mime="application/vnd.ms-excel"
+    )   
+
+st.set_page_config(page_title="EHR Data Transfer", page_icon="🖥️")
+st.markdown("# EHR Data Transfer")
+st.sidebar.header("")
+
+data_frame_demo()
